60e076dec632df96b7ac95a4e7283212ef14098a
nazim uddin <mdnajim1247@gmail.com>
2025-01-01 18:11:38 +0600

    first commit

--- client/src/pages/SignUp/SignUp.jsx
+++ client/src/pages/SignUp/SignUp.jsx
@@ -3,6 +3,7 @@ import { FcGoogle } from 'react-icons/fc'
 import useAuth from '../../hooks/useAuth'
 import { toast } from 'react-hot-toast'
 import { TbFidgetSpinner } from 'react-icons/tb'
+import { imageUpload } from '../../layouts/utils'
 
 const SignUp = () => {
   const { createUser, updateUserProfile, signInWithGoogle, loading } = useAuth()
@@ -15,6 +16,13 @@ const SignUp = () => {
     const email = form.email.value
     const password = form.password.value
 
+    const image = form.image.files[0]
+    console.log(image)
+    
+    // 1. send img data to imgbb
+    const photoUrl = await imageUpload(image)
+    
+    
     try {
       //2. User Registration
       const result = await createUser(email, password)
@@ -22,9 +30,9 @@ const SignUp = () => {
       //3. Save username & profile photo
       await updateUserProfile(
         name,
-        'https://lh3.googleusercontent.com/a/ACg8ocKUMU3XIX-JSUB80Gj_bYIWfYudpibgdwZE1xqmAGxHASgdvCZZ=s96-c'
+        photoUrl
       )
-      console.log(result)
+      console.log(result.user)
 
       navigate('/')
       toast.success('Signup Successful')
