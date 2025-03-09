d2428318b2f1c9022b0d1ca7a707895ce7d1bc86
nazim uddin <mdnajim1247@gmail.com>
2025-03-09 17:10:22 +0600

    data ready for purchess

--- client/src/components/Modal/PurchaseModal.jsx
+++ client/src/components/Modal/PurchaseModal.jsx
@@ -6,10 +6,58 @@ import {
   DialogPanel,
   DialogTitle,
 } from '@headlessui/react'
-import { Fragment } from 'react'
+import { Fragment, useEffect, useState } from 'react'
+import Button from '../Shared/Button/Button'
+import useAuth from '../../hooks/useAuth'
+import toast from 'react-hot-toast'
 
-const PurchaseModal = ({ closeModal, isOpen }) => {
+const PurchaseModal = ({ closeModal, isOpen, plant }) => {
+  const {user} = useAuth();
+  const {category,name,price,seller,quantity, _id} = plant || {};
+  const [totalPrice, setTotalPrice] = useState(price)
+  const [totalQuantity, setTotalQuantity] = useState(1);
+  const [parchesInfo, setParchesInfo] = useState(null)
+
+  useEffect(() => {
+    if (user) {
+      setParchesInfo({
+        customer: {
+          name: user?.displayName || "Unknown",
+          email: user?.email || "No Email",
+          image: user?.photoURL || "",
+        },
+        plantId: _id,
+        price: totalPrice,
+        quantity: totalQuantity,
+        seller: seller?.email || "No Seller",
+        address: '',
+        status: 'Pending..',
+      });
+    }
+  }, [user, _id, totalPrice, totalQuantity, seller]);
+
+
   // Total Price Calculation
+
+  const handleOrder = async ()=>{
+console.log(parchesInfo)
+  }
+
+  const handleQuantity = value =>{
+    if(value > quantity){
+      setTotalQuantity(quantity)
+      return toast.error('Quantity exceeds available stock')
+    }
+    if(value < 0){
+      setTotalQuantity(1)
+      return toast.error('Quantity can not be less than 1')
+    }
+    setTotalQuantity(value)
+    setTotalPrice(value * price)
+    setParchesInfo(prv =>{
+      return {...prv, quantity: value, price: value*price}
+    })
+  }
 
   return (
     <Transition appear show={isOpen} as={Fragment}>
@@ -45,20 +93,57 @@ const PurchaseModal = ({ closeModal, isOpen }) => {
                   Review Info Before Purchase
                 </DialogTitle>
                 <div className='mt-2'>
-                  <p className='text-sm text-gray-500'>Plant: Money Plant</p>
+                  <p className='text-sm text-gray-500'>Plant: {name}</p>
                 </div>
                 <div className='mt-2'>
-                  <p className='text-sm text-gray-500'>Category: Indoor</p>
+                  <p className='text-sm text-gray-500'>Category: {category}</p>
                 </div>
                 <div className='mt-2'>
-                  <p className='text-sm text-gray-500'>Customer: PH</p>
+                  <p className='text-sm text-gray-500'>Customer: {user?.displayName}</p>
                 </div>
 
                 <div className='mt-2'>
-                  <p className='text-sm text-gray-500'>Price: $ 120</p>
+                  <p className='text-sm text-gray-500'>Price: $ {price}</p>
                 </div>
                 <div className='mt-2'>
-                  <p className='text-sm text-gray-500'>Available Quantity: 5</p>
+                  <p className='text-sm text-gray-500'>Available Quantity: {quantity}</p>
+                  {/* Quantity */}
+              <div className='space-x-2 text-sm'>
+                <label htmlFor='quantity' className=' text-gray-600'>
+                  Quantity
+                </label>
+                <input
+                // max={quantity}
+                value={totalQuantity}
+                onChange={e => handleQuantity(parseInt(e.target.value))}
+                  className='p-1 mt-2 text-gray-800 border border-lime-300 focus:outline-lime-500 rounded-md bg-white'
+                  name='quantity'
+                  id='quantity'
+                  type='number'
+                  placeholder='Available quantity'
+                  required
+                />
+              </div>
+                  {/* Address */}
+              <div className='space-x-3 text-sm'>
+                <label htmlFor='quantity' className=' text-gray-600'>
+                  Address
+                </label>
+                <input
+                onChange={(e)=>setParchesInfo(prv =>{
+                  return {...prv, address: e.target.value}
+                })}
+                  className='p-1 mt-2 text-gray-800 border border-lime-300 focus:outline-lime-500 rounded-md bg-white'
+                  name='address'
+                  id='address'
+                  type='text'
+                  placeholder='Shipping Address'
+                  required
+                />
+              </div>
+              <div className='mt-2'>
+              <Button onClick={handleOrder} label={`Pay ${totalPrice} $`}/>
+              </div>
                 </div>
               </DialogPanel>
             </TransitionChild>
--- server/index.js
+++ server/index.js
@@ -114,7 +114,7 @@ async function run() {
     })
 
     // get a plant by id
-    app.get('plants/:id',async (req,res)=>{
+    app.get('/plants/:id',async (req,res)=>{
       const id = req.params.id;
       const query = {_id: new ObjectId(id)};
       const result = await plantsCollection.findOne(query);
